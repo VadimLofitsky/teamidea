48d7176a25ee4af68f589d5bd578175c1370b394
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-10-21 01:54:25 +0300

    init and task1

---
+++ task1/task1.sql
@@ -0,0 +1,8 @@
+select b.name brand, w.name whouse, sum(g.quantity) qty
+    from brand b, warehouse w left join goods g
+        on g.warehouseid=w.id
+    left join product p
+        on g.productid = p.id
+    where p.brandid=b.id
+    group by w.name, b.name
+    order by b.name asc, qty desc;
