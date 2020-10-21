e9e7c4b7000afa53733e0f95f9048747b7706922
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-10-22 02:47:54 +0300

    fix task1

--- task1/task1.sql
+++ task1/task1.sql
@@ -1,20 +1,23 @@
 -- список брендов с указанием количества единиц продукта текущего бренда на каждом складе
 -- с учётом возможности отсутствия товаров некоторых брендов на некоторых складах
-with all_pairs as (             -- все возможные пары бренд-склад
-        select b.name brand, w.name whouse
-            from brand b, warehouse w
+with all_pairs as (     -- все возможные пары бренд-склад
+    select b.name brand,
+           w.name whouse,
+           b.id brandid,
+           w.id whid
+        from brand b, warehouse w
     ),
     each_brand_per_wh as (      -- количество товара каждого имеющегося бренда на каждом складе
-        select b.name brand,
-                w.name whouse,
-                sum(g.quantity) qty
-            from brand b, warehouse w
+        select ap.brand brand,
+               ap.whouse whouse,
+               sum(g.quantity) qty
+            from all_pairs ap
             left outer join goods g
-                on g.warehouseid = w.id
+                on g.warehouseid = ap.whid
             left outer join product p
                 on g.productid = p.id
-            where p.brandid = b.id
-            group by w.name, b.name
+            where p.brandid = ap.brandid
+            group by ap.whouse, ap.brand
     )
 select ap.brand, ap.whouse, ebpw.qty
     from all_pairs ap
