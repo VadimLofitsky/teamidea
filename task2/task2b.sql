579029b3bfcfa1c6fc5dc51c291474223be86741
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-10-21 03:55:06 +0300

    task 2b

--- task2/task2b.sql
+++ task2/task2b.sql
@@ -1,3 +1,3 @@
-select product.name, brand.name
+select product.name product, brand.name brand
     from product join brand on product.brandid = brand.id
     where product.id not in (select distinct goods.productid from goods);
